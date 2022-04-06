8e8d9004e66f6f03a9f5ad3a1d2e3fb5f6b8116a
Frederique ROBIN <frederique.robin@edf.fr>
2022-04-06 18:59:09 +0200

    modify commander to save model

--- commander.py
+++ commander.py
@@ -8,7 +8,7 @@ logger = logging.getLogger()
 
 #### SG path
 import sys
-CAO_DIR = '/home/i37181/CAO_ML/git_depot/sketch_graph'
+CAO_DIR = '/home/i37181/Documents/Projets/CAO/SketchGraphs'
 sys.path.append(os.path.join(CAO_DIR, 'sketchgraphs'))
 
 ######## STEP 1 : Import Datasets
@@ -68,14 +68,23 @@ logger.info('-- Model initialization: end')
 # Initialize a trainer
 logger.info('-- Logger and Trainer initialization:...')
 from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.callbacks import ModelCheckpoint
 
 logger_conf = conf.get('logger')
 logger_tensorboard = TensorBoardLogger(save_dir = logger_conf.get('save_dir'), name = logger_conf.get('name'), log_graph=True)
-trainer = pl.Trainer(gpus=1, max_epochs=10, 
+
+
+checkpoint_callback = ModelCheckpoint(monitor='val_loss',
+                                        dirpath=logger_conf.get('save_dir'),
+                                        filename='gat-{epoch:02d}-{val_loss:.2f}',
+                                        mode="max",
+                                        save_weights_only=True)
+trainer = pl.Trainer(gpus=1, max_epochs=2, 
                     progress_bar_refresh_rate=20, 
                     logger=logger_tensorboard,
                     limit_train_batches=100,
-                    limit_val_batches=100
+                    limit_val_batches=100,
+                    callbacks=[checkpoint_callback]
                     )
 
 logger.info('-- Logger and Trainer initialization: end')
@@ -84,6 +93,10 @@ logger.info('-- Logger and Trainer initialization: end')
 logger.info('-- Model fit: ...')
 trainer.fit(sketchPredictionmodel, datamodule=data)
 logger.info('-- Model fit: Done')
+
+# model_scripted = torch.jit.script(model) # Export to TorchScript
+# model_scripted.save(os.path.join(logger_conf.get('save_dir'),'model_scripted.pt')) # Save
+torch.save(model.state_dict(), os.path.join(logger_conf.get('save_dir'),'model_scripted.pt'))
 
 ######## STEP 3 : Compute validation
 # trainer.test(test_dataloaders=test)
