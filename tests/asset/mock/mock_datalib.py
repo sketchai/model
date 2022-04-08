25109ec9ba8b00343227a8512654e851902f02f4
Frederique ROBIN <frederique.robin@edf.fr>
2022-04-08 17:48:31 +0200

    modify dataset

--- tests/asset/mock/mock_datalib.py
+++
@@ -1,9 +0,0 @@
-import enum
-
-
-class MockConstraintType(enum.IntEnum):
-    """This enumeration represents the type of the constraint."""
-    Coincident = 0
-    Diameter = 12
-    Angle = 17
-    Unknown = 29
