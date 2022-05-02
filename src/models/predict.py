d9541d3f0295d100749c245c6414a77b4f947d3a
Julien Moreau <julien-j.moreau@edf.fr>
2022-05-02 16:42:49 +0200

    add max_size to embeddings

--- src/models/predict.py
+++ src/models/predict.py
@@ -104,28 +104,43 @@ class PredictSketch(pl.LightningModule):
             disp.plot(ax=ax,xticks_rotation='vertical',include_values=False,)
             tensorboard.add_figure('confusion_matrix',fig,global_step=self.global_step)
 
-    def log_embeddings(self, batch,tag=''):
+    def log_embeddings(self, batch,tag='',max_size=1000):
+        node_features = batch['node_features'].detach().cpu().numpy()
+        edge_features = batch['edge_features'].detach().cpu().numpy()
         embeddings = self.model.embeddings(batch)
+
+        # limit number of data points to display
+        if max_size is not None:
+            for key, value in embeddings.items():
+                embeddings[key] = value[:max_size]
+            node_features = node_features[:max_size]
+            edge_features = edge_features[:max_size]
+
         tensorboard = self.logger.experiment
         
         node_embeddings = {k:v for k,v in embeddings.items() if 'node' in k}
         node_inverse_map = {i: t for t, i in self.node_idx_map.items()}
-        node_label_names = [node_inverse_map[k] for k in batch['node_features'].detach().cpu().numpy()]
+        node_label_names = [node_inverse_map[k] for k in node_features]
         for key, array in node_embeddings.items():
-            tensorboard.add_embedding(
-                array,metadata=node_label_names,tag=f'{tag}/epoch_{self.current_epoch}/{key}')
+            tensorboard.add_embedding(array,metadata=node_label_names,
+                global_step=self.global_step,
+                tag=f'epoch_{self.current_epoch:03d}/{key}')
 
         key = 'edges_bf_msg_passing'
         array = embeddings[key]
         edge_inverse_map = {i: t for t, i in self.edge_idx_map.items()}
-        edge_label_names = [edge_inverse_map[k] for k in batch['edge_features'].detach().cpu().numpy()]
-        tensorboard.add_embedding(array,metadata=edge_label_names,tag=f'{tag}/epoch_{self.current_epoch}/{key}')
+        edge_label_names = [edge_inverse_map[k] for k in edge_features]
+        tensorboard.add_embedding(array,metadata=edge_label_names,
+            global_step=self.global_step,
+            tag=f'epoch_{self.current_epoch:03d}/{key}')
 
         pos_embedd = embeddings['edges_pos_after_transformer']
         neg_embedd = embeddings['edges_neg_after_transformer']
         array = np.concatenate([pos_embedd, neg_embedd], axis=0)
         edge_label = ['pos']*pos_embedd.shape[0] + ['neg']*neg_embedd.shape[0]
-        tensorboard.add_embedding(array,metadata=edge_label,tag=f'{tag}/epoch_{self.current_epoch}/binary_edge_classification')
+        tensorboard.add_embedding(array,metadata=edge_label,
+            global_step=self.global_step,
+            tag=f'epoch_{self.current_epoch:03d}/binary_edge_classification')
 
         
         key = 'edges_pos_after_transformer'
@@ -133,4 +148,6 @@ class PredictSketch(pl.LightningModule):
         edge_inverse_map = {i: t for t, i in self.edge_idx_map.items()}
         print(embeddings['inferred_edges_pos_type'].shape)
         edge_label_names = [edge_inverse_map[k] for k in embeddings['inferred_edges_pos_type']]
-        tensorboard.add_embedding(array,metadata=edge_label_names,tag=f'{tag}/epoch_{self.current_epoch}/edge_type_inference')
+        tensorboard.add_embedding(array,metadata=edge_label_names,
+            global_step=self.global_step,
+            tag=f'epoch_{self.current_epoch:03d}/edge_type_inference')
