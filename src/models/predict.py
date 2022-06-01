ecc1acbbb554df8a545f85511a08ceab3283d975
Julien Moreau <julien-j.moreau@edf.fr>
2022-06-01 17:17:49 +0200

    refactor confusion matrix

--- src/models/predict.py
+++ src/models/predict.py
@@ -52,11 +52,11 @@ class PredictSketch(pl.LightningModule):
         if batch_idx%100 == 0:
             edges_pos = prediction['edges_pos'].cpu().detach().numpy()
             edges_neg = prediction['edges_neg'].cpu().detach().numpy()
-            self.log_binary_classification(edges_pos, edges_neg, tag='train',batch_idx=batch_idx)
+            self.log_binary_classification(edges_pos, edges_neg, tag='train')
             #TODO add aggregation (for now only one batch is used to compute the curve)
             frequency = self.max_epochs//10
             if batch_idx==0 and self.current_epoch%frequency==0:
-                self.log_pr_curve(self, edges_pos, edges_neg, tag='train')
+                self.log_pr_curve(edges_pos, edges_neg, tag='train')
         return loss 
 
 
@@ -76,20 +76,20 @@ class PredictSketch(pl.LightningModule):
         self.log('val/loss', loss)
         self.log('hp/val_loss', loss)
 
-        self.log_binary_classification(edges_pos, edges_neg, tag='val',batch_idx=batch_idx)
-
-        self.log_multiclass(true_type, predicted_type_pos, tag='val',batch_idx=batch_idx)
+        self.log_binary_classification(edges_pos, edges_neg, tag='val')
+        self.log_multiclass(true_type, predicted_type_pos, tag='val')
 
         if (self.current_epoch in [5,15,49]) and batch_idx==0:
             self.log_embeddings(batch, tag='val')
 
+        #TODO add aggregation (for now only one batch is used to compute the matrix)
         #TODO add aggregation (for now only one batch is used to compute the curve)
         frequency = self.max_epochs//10
         if batch_idx==0 and self.current_epoch%frequency==0:
-            self.log_pr_curve(self, edges_pos, edges_neg, tag='val')
+            self.log_pr_curve(edges_pos, edges_neg, tag='val')
+            self.log_confusion_matrix(true_type, predicted_type_pos, tag='val')
 
-    def log_binary_classification(self, edges_pos, edges_neg, tag, batch_idx, on_step=False, on_epoch=True):
-        tensorboard = self.logger.experiment
+    def log_binary_classification(self, edges_pos, edges_neg, tag, on_step=False, on_epoch=True):
         tp = np.sum(edges_pos > 0)
         fn = np.sum(edges_pos < 0)
         tn = np.sum(edges_neg < 0)
@@ -104,26 +104,27 @@ class PredictSketch(pl.LightningModule):
             self.log(f'{tag}/bin_recall',recall, on_step=on_step, on_epoch=on_epoch)
 
     def log_pr_curve(self, edges_pos, edges_neg, tag):
+        tb_logger = self.logger.experiment
         label = np.concatenate([np.ones([len(edges_pos)]), np.zeros([len(edges_neg)])], axis = 0)
         scalar_pred = np.concatenate([edges_pos,edges_neg], axis = 0).flatten()
         predictions = 1 / (1 + np.exp(-scalar_pred))
-        tensorboard.add_pr_curve(f'{tag}/pr_curve', label, predictions,global_step=self.global_step)
+        tb_logger.add_pr_curve(f'{tag}/pr_curve', label, predictions,global_step=self.global_step)
 
-    def log_multiclass(self,true_type, predicted_type, tag, batch_idx):
-        tensorboard = self.logger.experiment
-        label_names = list(self.edge_idx_map.keys())[:-1]
-
+    def log_multiclass(self,true_type, predicted_type, tag):
         accuracy = np.mean(predicted_type==true_type)
         self.log(f'{tag}/class_accuracy', accuracy)
-        #TODO add aggregation (for now only one batch is used to compute the matrix)
-        if confusion_matrix is not None and batch_idx==0:
+
+    def log_confusion_matrix(self,true_type, predicted_type, tag):
+        tb_logger = self.logger.experiment
+        label_names = list(self.edge_idx_map.keys())[:-1]
+        if confusion_matrix is not None:
             cm = confusion_matrix(true_type, predicted_type, labels=np.arange(len(label_names)), normalize='pred')
             disp = ConfusionMatrixDisplay(
                 confusion_matrix=cm,
                 display_labels=label_names)
             fig, ax = plt.subplots()
             disp.plot(ax=ax,xticks_rotation='vertical',include_values=False,)
-            tensorboard.add_figure(f'{tag}/confusion_matrix',fig,global_step=self.global_step)
+            tb_logger.add_figure(f'{tag}/confusion_matrix',fig,global_step=self.global_step)
 
     def log_embeddings(self, batch,tag='',max_size=1000):
         node_features = batch['node_features'].detach().cpu().numpy()
@@ -137,13 +138,13 @@ class PredictSketch(pl.LightningModule):
             node_features = node_features[:max_size]
             edge_features = edge_features[:max_size]
 
-        tensorboard = self.logger.experiment
+        tb_logger = self.logger.experiment
         
         node_embeddings = {k:v for k,v in embeddings.items() if 'node' in k}
         node_inverse_map = {i: t for t, i in self.node_idx_map.items()}
         node_label_names = [node_inverse_map[k] for k in node_features]
         for key, array in node_embeddings.items():
-            tensorboard.add_embedding(array,metadata=node_label_names,
+            tb_logger.add_embedding(array,metadata=node_label_names,
                 global_step=self.global_step,
                 tag=f'epoch_{self.current_epoch:03d}/{key}')
 
@@ -151,7 +152,7 @@ class PredictSketch(pl.LightningModule):
         array = embeddings[key]
         edge_inverse_map = {i: t for t, i in self.edge_idx_map.items()}
         edge_label_names = [edge_inverse_map[k] for k in edge_features]
-        tensorboard.add_embedding(array,metadata=edge_label_names,
+        tb_logger.add_embedding(array,metadata=edge_label_names,
             global_step=self.global_step,
             tag=f'epoch_{self.current_epoch:03d}/{key}')
 
@@ -159,7 +160,7 @@ class PredictSketch(pl.LightningModule):
         neg_embedd = embeddings['edges_neg_after_transformer']
         array = np.concatenate([pos_embedd, neg_embedd], axis=0)
         edge_label = ['pos']*pos_embedd.shape[0] + ['neg']*neg_embedd.shape[0]
-        tensorboard.add_embedding(array,metadata=edge_label,
+        tb_logger.add_embedding(array,metadata=edge_label,
             global_step=self.global_step,
             tag=f'epoch_{self.current_epoch:03d}/binary_edge_classification')
 
@@ -169,6 +170,6 @@ class PredictSketch(pl.LightningModule):
         edge_inverse_map = {i: t for t, i in self.edge_idx_map.items()}
         print(embeddings['inferred_edges_pos_type'].shape)
         edge_label_names = [edge_inverse_map[k] for k in embeddings['inferred_edges_pos_type']]
-        tensorboard.add_embedding(array,metadata=edge_label_names,
+        tb_logger.add_embedding(array,metadata=edge_label_names,
             global_step=self.global_step,
             tag=f'epoch_{self.current_epoch:03d}/edge_type_inference')
